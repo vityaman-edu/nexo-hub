cac1c5501b2f4f6298d524746eee4ef9e86ebd4a
Andryss <andr.krivosheev@gmail.com>
2024-03-07 13:52:00 +0300

    Add load test

--- log-service/src/test/kotlin/ApplicationKtTest.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
-
-internal class ApplicationKtTest {
-    @Test
-    fun testMain() {
-        assertDoesNotThrow(::main)
-    }
-}
---
+++ log-service/src/test/kotlin/org/vivlaniv/nexohub/ApplicationKtTest.kt
@@ -0,0 +1,33 @@
+package org.vivlaniv.nexohub
+
+import org.redisson.Redisson
+import org.redisson.config.Config
+import java.time.Instant
+
+fun main() {
+    testLoad()
+}
+
+fun testLoad() {
+    val config = Config()
+    config.useSingleServer().address = "redis://localhost:6379"
+    val redissonClient = Redisson.create(config)
+    for (i in 0 until 20) {
+        Thread{
+            for (j in 0 until 100000) {
+                redissonClient.getTopic("logs").publish(LogMessage(Instant.now(), genMsg()))
+            }
+            println("thread finished")
+        }.start()
+    }
+
+    println("all request sent")
+}
+
+private fun genMsg(): String {
+    val chars = CharArray(1024)
+    for (i in chars.indices) {
+        chars[i] = 'a' + (Math.random() * 26).toInt()
+    }
+    return String(chars)
+}
