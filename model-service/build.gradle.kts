da017bd667969dcc8593b4245b8f29b431e21ad8
Andryss <andr.krivosheev@gmail.com>
2024-03-12 18:22:09 +0300

    Initial commit

---
+++ model-service/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.vivlaniv.nexohub"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.redisson:redisson:3.21.3")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
